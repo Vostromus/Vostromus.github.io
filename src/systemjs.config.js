431ad8a17f81246e3e3597ed6ee258fe6515fbc0
Vostromus <vostromus@gmail.com>
2017-04-22 22:39:26 -0400

    update url try 8

--- src/systemjs.config.js
+++ src/systemjs.config.js
@@ -50,6 +50,7 @@
       },
       lodash : { main:'lodash.js', defaultExtension:'js'}
     },
-    baseURL: 'Vostromus.github.io/'
+    baseURL: 'Vostromus.github.io/',
+    exclude:[]
   });
 })(this);
